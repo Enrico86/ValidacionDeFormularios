27a95209c29c2f96752b0bbaa3f8823d9ace5933
Enrico86 <64355652+Enrico86@users.noreply.github.com>
2020-10-05 09:07:03 +0200

    Simpleas validations: Code Behind Validations y Behaviors Validations

---
+++ ValidacionDeFormularios/ValidacionDeFormularios/Behaviors/EmailBehavior.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ValidacionDeFormularios.Utilities;
+using Xamarin.Forms;
+
+namespace ValidacionDeFormularios.Behaviors
+{
+    public class EmailBehavior: Behavior<Entry>
+    {
+        public static readonly BindableProperty errorBackgroundColorProperty
+            = BindableProperty.Create
+            (
+                "errorBackgroundColor",
+                typeof(Color),
+                typeof(EmailBehavior),
+                Color.Default
+            );
+        public static readonly BindableProperty errorTextColorProperty
+            = BindableProperty.Create("errorTextColor",typeof(Color),typeof(EmailBehavior),Color.Default);
+        public static readonly BindableProperty validBackgroundColorProperty
+            = BindableProperty.Create("validBackgroundColor", typeof(Color), typeof(EmailBehavior), Color.Default);
+        public static readonly BindableProperty validTextColorProperty
+            = BindableProperty.Create("validTextColor", typeof(Color), typeof(EmailBehavior), Color.Default);
+
+        public Color ErrorBackgroundColor
+        {
+            get { return (Color)GetValue(errorBackgroundColorProperty); }
+            set { SetValue(errorBackgroundColorProperty, value); }
+        }
+
+        public Color ErrorTextColor
+        {
+            get { return (Color)GetValue(errorTextColorProperty); }
+            set { SetValue(errorTextColorProperty,value); }
+        }
+
+        public Color ValidBackgroundColor
+        {
+            get { return (Color)GetValue(validBackgroundColorProperty); }
+            set { SetValue(validBackgroundColorProperty, value); }
+        }
+
+        public Color ValidTextColor
+        {
+            get { return (Color)GetValue(validTextColorProperty); }
+            set { SetValue(validTextColorProperty, value); }
+        }
+
+        static readonly BindablePropertyKey IsValidPropertyKey =
+            BindableProperty.CreateReadOnly("IsValid", typeof(bool), typeof(EmailBehavior), false);
+        public static readonly BindableProperty IsValidProperty =
+            IsValidPropertyKey.BindableProperty;
+        public bool IsValid
+        {
+            get { return (bool)GetValue(IsValidProperty); }
+            set { SetValue(IsValidPropertyKey, value); }
+        }
+
+
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            base.OnAttachedTo(bindable);
+            bindable.TextChanged += Bindable_TextChanged;
+        }
+
+        private void Bindable_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var email = e.NewTextValue;
+            var entry = sender as Entry;
+            IsValid = RegexUtilities.IsValidEmail(email);
+            entry.BackgroundColor = RegexUtilities.IsValidEmail(email) ? ValidBackgroundColor : ErrorBackgroundColor;
+            entry.TextColor = RegexUtilities.IsValidEmail(email) ? ValidTextColor : ErrorTextColor;
+        }
+
+        protected override void OnDetachingFrom(Entry bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            bindable.TextChanged -= Bindable_TextChanged;
+        }
+    }
+}
---
+++ ValidacionDeFormularios/ValidacionDeFormularios/Views/SimpleValidation/CodeBehindValidation.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ValidacionDeFormularios.Utilities;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ValidacionDeFormularios.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CodeBehindValidation : ContentPage
+    {
+        public CodeBehindValidation()
+        {
+            InitializeComponent();
+        }
+
+        private void loginButton_Clicked(object sender, EventArgs e)
+        {
+            if (RegexUtilities.IsValidEmail(emailEntry.Text))
+            {
+                errorLabel.Text = "Email válido";
+            }
+            else errorLabel.Text = "Email incorrecto";
+        }
+
+        private void emailEntry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var email = e.NewTextValue;
+            var entry = sender as Entry;
+            if (RegexUtilities.IsValidEmail(email))
+            {
+                errorLabel.Text = "Email válido";
+                entry.BackgroundColor = Color.LightGreen;
+                entry.TextColor = Color.DarkGreen;
+            }
+            else
+            {
+                errorLabel.Text = "Email incorrecto";
+                entry.BackgroundColor = Color.LightCoral;
+                entry.TextColor = Color.DarkRed;
+            }
+
+        }
+    }
+}
